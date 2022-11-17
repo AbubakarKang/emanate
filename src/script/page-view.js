816fe511cb5613c9070cb6cf3a7a497b97fdf6dd
AbubakarKang <kangabubakar33@gmail.com>
2022-11-17 10:09:16 +0800

    🏗️ Added pages

--- src/script/page-view.js
+++ src/script/page-view.js
@@ -1,11 +1,41 @@
+//-----------------------------\\ ELEMENTS //-----------------------------\\
+
+const discoverNavBtn = document.querySelector("[data-discover-nav]");
+const settingsNavBtn = document.querySelector("[data-settings-nav]");
+const cliqueNavBtn = document.querySelector("[data-clique-nav]");
+const chatNavBtn = document.querySelector("[data-chat-nav]");
+
+const chatPage = document.querySelector("[data-chat-page]");
+const cliquePage = document.querySelector("[data-clique-page]");
+const settingsPage = document.querySelector("[data-settings-page]");
+const discoverPage = document.querySelector("[data-discover-page]");
+
 //-----------------------------\\ FUNCTIONS //-----------------------------\\
 
-document.querySelector("[data-homepage-nav]").addEventListener("click", () => {
-	document.querySelector("[data-homepage-page]").style.zIndex = "2";
-	document.querySelector("[data-test-page]").style.zIndex = "1";
+cliqueNavBtn.addEventListener("click", () => {
+	cliquePage.style.zIndex = "3";
+	chatPage.style.zIndex = "1";
+	discoverPage.style.zIndex = "1";
+	settingsPage.style.zIndex = "1";
 });
 
-document.querySelector("[data-test-nav]").addEventListener("click", () => {
-	document.querySelector("[data-test-page]").style.zIndex = "2";
-	document.querySelector("[data-homepage-page]").style.zIndex = "1";
+chatNavBtn.addEventListener("click", () => {
+	chatPage.style.zIndex = "3";
+	cliquePage.style.zIndex = "1";
+	discoverPage.style.zIndex = "1";
+	settingsPage.style.zIndex = "1";
+});
+
+discoverNavBtn.addEventListener("click", () => {
+	discoverPage.style.zIndex = "3";
+	settingsPage.style.zIndex = "1";
+	cliquePage.style.zIndex = "1";
+	chatPage.style.zIndex = "1";
+});
+
+settingsNavBtn.addEventListener("click", () => {
+	settingsPage.style.zIndex = "3";
+	discoverPage.style.zIndex = "1";
+	cliquePage.style.zIndex = "1";
+	chatPage.style.zIndex = "1";
 });
